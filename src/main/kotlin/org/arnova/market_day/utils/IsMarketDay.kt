aea080ec0f7556d17e17042196b4794f2bb37d4f
Arnova-1 <arnova124@gmail.com>
2026-01-14 20:28:02 +0700

    add day counter logic

---
+++ src/main/kotlin/org/arnova/market_day/utils/IsMarketDay.kt
@@ -0,0 +1,24 @@
+package org.arnova.market_day.utils
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.minecraft.world.World
+
+object MarketTime {
+    var lastDay = -1
+    var marketDayCounter = 0
+
+    fun isMarketDay(world: World): Boolean {
+        ServerTickEvents.END_WORLD_TICK.register { world ->
+            val currentDay = (world.timeOfDay / 24000).toInt()
+
+            if (currentDay != lastDay) {
+                marketDayCounter++
+                lastDay = currentDay
+            }
+        }
+
+        println("Day $marketDayCounter")
+
+        return marketDayCounter % 3 != 0
+    }
+}
