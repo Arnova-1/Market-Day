da393b9467745ef6c068364fc4ef0cb1fee7259d
Arnova-1 <arnova124@gmail.com>
2026-01-14 20:29:10 +0700

    intercept right click on villager

--- src/main/kotlin/org/arnova/market_day/Market_day.kt
+++ src/main/kotlin/org/arnova/market_day/Market_day.kt
@@ -1,9 +1,22 @@
 package org.arnova.market_day
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.player.UseEntityCallback
+import net.minecraft.entity.passive.VillagerEntity
+import net.minecraft.util.ActionResult
+import org.arnova.market_day.utils.MarketTime.isMarketDay
 
 class MarketDay : ModInitializer {
 
     override fun onInitialize() {
+        UseEntityCallback.EVENT.register { player, world, hand, entity, hitResult ->
+            if (world.isClient) {
+                return@register ActionResult.PASS
+            }
+            if (entity is VillagerEntity && isMarketDay(world)) {
+                ActionResult.FAIL
+            } else
+                ActionResult.PASS
+        }
     }
 }
