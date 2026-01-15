9a5c70fdf5ebc4189dd4f047dd89d0dd84c76881
Arnova-1 <arnova124@gmail.com>
2026-01-15 17:12:57 +0700

    refactor: initialize market system in entrypoint

--- src/main/kotlin/org/arnova/market_day/Market_day.kt
+++ src/main/kotlin/org/arnova/market_day/Market_day.kt
@@ -4,23 +4,27 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.player.UseEntityCallback
 import net.minecraft.entity.passive.VillagerEntity
 import net.minecraft.util.ActionResult
-import org.arnova.market_day.utils.MarketTime.isMarketDay
+import org.arnova.market_day.utils.MarketDayAnnouncement
+import org.arnova.market_day.utils.MarketTime
 import org.arnova.market_day.utils.getClosedMarketMessage
 
 class MarketDay : ModInitializer {
-
     override fun onInitialize() {
+        MarketTime.init()
+        MarketDayAnnouncement.init()
+
         UseEntityCallback.EVENT.register { player, world, hand, entity, hitResult ->
             if (world.isClient) {
                 return@register ActionResult.PASS
             }
-            if (entity is VillagerEntity && isMarketDay(world)) {
+            if (entity is VillagerEntity && MarketTime.isMarketDay()) {
                 player.sendMessage(
                     getClosedMarketMessage(entity),
                     true)
                 ActionResult.FAIL
-            } else
+            } else {
                 ActionResult.PASS
+            }
         }
     }
 }
